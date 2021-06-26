5eb3721129378b140ca45f0a504fb308074d7fa5
DaeseongLee <hsl5539@gmail.com>
2021-06-27 05:56:11 +0900

    Adapt tailwindCss

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다!!`);
     error.status = 404;
     next(error);
 });
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello User');
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/join', (req, res) => {
+    res.render('join');
 });
 
 module.exports = router;
