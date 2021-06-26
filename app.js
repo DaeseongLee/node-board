1a6d93420095f9dfc19431809dae3d92e3854563
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-06-27 02:24:37 +0900

    Add error middleware

--- app.js
+++ app.js
@@ -39,12 +39,6 @@ app.use(session({
     name: 'session-cookie',
 }));
 
-app.use((req, res, next) => {
-    console.log(req.session);
-    console.log(req.signedCookies);
-    next();
-})
-
 
 app.use('/user', userRouter);
 app.use('/board', boardRouter);
@@ -54,9 +48,17 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Express' });
 });
 
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
 app.use((error, req, res, next) => {
-    console.error(error);
-    res.status(500).send(err.message);
+    res.locals.message = error.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? error : {};
+    res.status(error.status || 500);
+    res.render('error');
 })
 
 
