b9117c172487ec2614947dd62081c14ec64a5ec1
DaeseongLee <hsl5539@gmail.com>
2021-06-27 21:33:49 +0900

    Update style

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello User');
+    res.render('board');
 });
 
+router.get('/create', (req, res) => {
+    res.render('createBoard');
+})
+
+router.get('/update/:id', (req, res) => {
+    res.render('boardUpdate');
+})
+
+router.get('/delete/:id', (req, res) => {
+    res.render('boardDelete');
+})
+
+router.get('/:id', (req, res) => {
+    res.render('boardDetail');
+})
 module.exports = router;
