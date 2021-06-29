c5fe8586cc2b959baffffb8c4711e0fbcd5a97a9
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-06-29 23:09:49 +0900

    Set config when  NODE_ENV=production

--- schema/index.js
+++ schema/index.js
@@ -5,17 +5,35 @@ const connect = () => {
         mongoose.set('debug', true);
     }
 
-    mongoose.connect('mongodb://localhost:27017/admin', {
-        dbName: 'spartaNode1',
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    }, (error) => {
-        if (error) {
-            console.log('몽고디비 연결 에러', error);
-        } else {
-            console.log('몽고디비 연결 성공');
-        }
-    });
+    console.log("env!!!!!!!!!!!!!!!!!!", process.env.NODE_ENV)
+    if (process.env.NODE_ENV == 'production') {
+        mongoose.connect('mongodb://localhost:27017/admin', {
+            dbName: 'spartaNode1',
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            user: "test",
+            pass: "test"
+        }, (error) => {
+            if (error) {
+                console.log('몽고디비 연결 에러', error);
+            } else {
+                console.log('몽고디비 연결 성공 production');
+            }
+        });
+    } else {
+        console.log("여기는 dev");
+        mongoose.connect('mongodb://localhost:27017/admin', {
+            dbName: 'spartaNode1',
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        }, (error) => {
+            if (error) {
+                console.log('몽고디비 연결 에러', error);
+            } else {
+                console.log('몽고디비 연결 성공');
+            }
+        });
+    }
 };
 
 mongoose.connection.on('error', (error) => {
