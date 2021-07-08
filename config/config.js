13fdc200462c2a516fe7db62a17256f4271137ea
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-07-08 15:15:51 +0900

    Add sequelize config setting in production

--- config/config.js
+++ config/config.js
@@ -3,23 +3,24 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: "root",
-    password: "1234",
+    password: process.env.DB_PASSWORD,
     database: "nodeboard",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: null,
-    database: "database_test",
+    password: process.env.DB_PASSWORD,
+    database: "nodeboard",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: "root",
-    password: null,
-    database: "database_production",
+    password: process.env.DB_PASSWORD,
+    database: "nodeboard",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false,
   }
 }
